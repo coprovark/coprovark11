d446481ffea40581609441167d8c5eaa5c30fc5e
chananporn <chananporn.kcs59@ubru.ac.th>
2018-03-07 21:17:02 +0700

    การบ้าน

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,4 +1,10 @@
 <h1>Profile</h1>
+<!DOCTYPE html>
+<html>
+<body>
+    <img src="http://images.clipartpanda.com/happy-computer-user-content-computer.jpg" style="height:300px;">
+</body>
+</html>
 <h3>
 ชื่อ นายชนันพร   ขันแก้ว<br>
 ชื่อเล่น ทิว<br>
